65c7b4f85a249a30b2c8eac9e700afaf933d8d20
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 11:56:01 +0800

    清楚多余模板

--- templates/admin/store_lock.dwt.php
+++ templates/admin/store_lock.dwt.php
@@ -1,202 +1,61 @@
 <?php defined('IN_ECJIA') or exit('No permission resources.');?>
 <!-- {extends file="ecjia.dwt.php"} -->
+
 <!-- {block name="footer"} -->
 <script type="text/javascript">
-	ecjia.admin.store_edit.init();
+	ecjia.admin.admin_config.init();
 </script>
 <!-- {/block} -->
 
 <!-- {block name="main_content"} -->
+
+<div class="alert alert-info">
+	<a class="close" data-dismiss="alert">×</a>
+	<strong>温馨提示：</strong>{t}这里的后台设置仅限改善商家后台的显示效果。{/t}
+</div>
+
 <div>
 	<h3 class="heading">
-		<!-- {if $ur_here}{$ur_here}{/if} -->
-		<!-- {if $action_link} -->
-		<a class="data-pjax btn plus_or_reply" id="sticky_a" href="{$action_link.href}"><i class="fontello-icon-reply"></i>{$action_link.text}</a>
-		<!-- {/if} -->
+		{if $ur_here}{$ur_here}{/if}
+		{if $action_link}
+		<a href="{$action_link.href}" class="btn data-pjax"  style="float:right;margin-top:-3px;"><i class="fontello-icon-reply"></i>{$action_link.text}</a>
+		{/if}
 	</h3>
 </div>
 
-<div class="row-fluid">
-	<div class="span12">
-		<form class="form-horizontal">
-			<fieldset>
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.companyname_lable'}</label>
-					<div class="controls l_h30">
-						{$store.company_name}
+<form class="form-horizontal"  name="theForm" action="{$form_action}" method="post"  enctype="multipart/form-data" >
+	<fieldset>
+		<div class="control-group formSep">
+			<label class="control-label">{t}后台名称：{/t}</label>
+			<div class="controls ">
+				<input name="merchante_admin_cpname" id="merchante_admin_cpname" type="text" value="{$config_cpname}"/>
+			</div>
+		</div>
+
+		<div class="control-group formSep">
+			<label class="control-label">{t}登录Logo：{/t}</label>
+			<div class="controls">
+				<div class="fileupload {if $config_logo}fileupload-exists{else}fileupload-new{/if}" data-provides="fileupload">
+					<div class="fileupload-preview thumbnail fileupload-exists" style="width: 50px; height: 50px; line-height: 50px;">
+						<img src="{$config_logoimg}" alt="{t}预览图片{/t}" />
 					</div>
+					<span class="btn btn-file">
+					<span class="fileupload-new">{t}浏览{/t}</span>
+					<span class="fileupload-exists">{t}修改{/t}</span>
+					<input type="file" name="merchante_admin_login_logo"/>
+					</span>
+					<a class="btn fileupload-exists" data-toggle="removefile" data-msg="{t}您确定要删除此文件吗？{/t}" data-href="{RC_Uri::url('seller/admin_config/del')}" {if $config_logo}data-removefile="true"{/if}>{t}删除{/t}</a>
 				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.person_lable'}</label>
-					<div class="controls l_h30">
-						{$store.responsible_person}
-					</div>
-				</div>
-			
-				<div class="control-group formSep" >
-					<label class="control-label">{lang key='store::store.identity_type_lable'}</label>
-					<div class="controls l_h30">
-						{$store.identity_type}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.business_licence_pic_lable'}</label>
-					<div class="controls">
-						<div class="fileupload fileupload-new" data-provides="fileupload">
-							{if $store.business_licence_pic neq ''}
-								<img class="w120 h120"  class="img-polaroid" src="{RC_Upload::upload_url()}/{$store.business_licence_pic}"><br>
-								{lang key='store::store.file_address'}{$store.business_licence_pic}<br>
-							{else}
-								<div class="l_h30">
-									{lang key='store::store.no_upload'}
-								</div>
-							{/if}
-						</div>
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.business_licence_lable'}</label>
-					<div class="controls l_h30">
-						{$store.business_licence}
-					</div>
-				</div>
+			</div>
+		</div>
 		
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.identity_pic_front_lable'}</label>
-					<div class="controls">
-						<div class="fileupload fileupload-new" data-provides="fileupload">
-							{if $store.business_licence_pic neq ''}
-								<img class="w120 h120"  class="img-polaroid" src="{RC_Upload::upload_url()}/{$store.identity_pic_front}"><br>
-								{lang key='store::store.file_address'}{$store.identity_pic_front}<br>
-							{else}
-								<div class="l_h30">
-									{lang key='store::store.no_upload'}
-								</div>
-							{/if}
-						</div>
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.identity_pic_back_lable'}</label>
-					<div class="controls">
-						<div class="fileupload fileupload-new" data-provides="fileupload">
-							{if $store.business_licence_pic neq ''}
-								<img class="w120 h120"  class="img-polaroid" src="{RC_Upload::upload_url()}/{$store.identity_pic_back}"><br>
-								{lang key='store::store.file_address'}{$store.identity_pic_back}<br>
-							{else}
-								<div class="l_h30">
-									{lang key='store::store.no_upload'}
-								</div>
-							{/if}
-						</div>
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.identity_number_lable'}</label>
-					<div class="controls l_h30">
-						{$store.identity_number}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.bank_name_lable'}</label>
-					<div class="controls l_h30">
-						{$store.bank_name}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.bank_branch_name_lable'}</label>
-					<div class="controls l_h30">
-						{$store.bank_branch_name}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.bank_account_number_lable'}</label>
-					<div class="controls l_h30">
-						{$store.bank_account_number}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.bank_address_lable'}</label>
-					<div class="controls l_h30">
-						{$store.bank_address}
-					</div>
-				</div>
-				
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.email_lable'}</label>
-					<div class="controls l_h30">
-						{$store.email}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.contact_lable'}</label>
-					<div class="controls l_h30">
-						{$store.contact_mobile}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.address_lable'}</label>
-					<div class="controls l_h30">
-						{$store.address}
-					</div>
-				</div>
-			
-				<div class="control-group formSep" >
-					<label class="control-label">{lang key='store::store.store_cat_lable'}</label>
-					<div class="controls l_h30">
-						{$store.store_cat}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.store_title_lable'}</label>
-					<div class="controls l_h30">
-						{$store.merchants_name}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.store_keywords_lable'}</label>
-					<div class="controls l_h30">
-						{$store.shop_keyword}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.longitude_lable'}</label>
-				 	<div class="controls l_h30">
-						{$store.longitude}
-					</div>
-				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">{lang key='store::store.latitude_lable'}</label>
-				 	<div class="controls l_h30">
-						{$store.latitude}
-					</div>
-				</div>
-				
-				<div class="control-group">
-					<label class="control-label">{lang key='store::store.apply_time_lable'}</label>
-				 	<div class="controls l_h30">
-						{$store.apply_time}
-					</div>
-				</div>
-				
-			</fieldset>
-		</form>
-	</div>
-</div>
+		<div class="control-group">
+			<div class="controls">
+				<input type="submit" value="{$lang.button_submit}" class="btn btn-gebo" />
+			</div>
+		</div>	 
+
+	</fieldset>
+</form>
+	        
 <!-- {/block} -->
