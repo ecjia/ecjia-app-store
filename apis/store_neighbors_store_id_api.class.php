793351ff506402cdd8a25d65244fa19a68d9d9cb
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-18 14:06:27 +0800

    修改商品列表api

--- apis/store_neighbors_store_id_api.class.php
+++ apis/store_neighbors_store_id_api.class.php
@@ -2,12 +2,12 @@
 defined('IN_ECJIA') or exit('No permission resources.');
 /**
  * 店铺列表接口
- * @author 
+ * @author
  *
  */
 class store_neighbors_store_id_api extends Component_Event_Api {
 	/**
-	 * 
+	 *
 	 * @param array $options
 	 * @return  array
 	 */
@@ -16,10 +16,10 @@ class store_neighbors_store_id_api extends Component_Event_Api {
 		) {
 			return new ecjia_error('invalid_parameter', RC_Lang::get('system::system.invalid_parameter'));
 		}
-		
+
 		return $this->neighbors_store($options['geohash']);
 	}
-	
+
 	/**
 	 *  获取经纬度周边店铺id
 	 *
@@ -31,21 +31,20 @@ class store_neighbors_store_id_api extends Component_Event_Api {
 	{
 		/* 载入geohash类*/
 		$geohash	  = RC_Loader::load_app_class('geohash', 'store');
-	
+
 		/* 获取当前经纬度周边的geohash值*/
 		$geohash_group = $geohash->geo_neighbors($geohash_code);
-	
+
 		$group_store_id = RC_DB::table('store_franchisee')->where('geohash', 'like', $geohash_code.'%')->lists('store_id');
-	
+
 		foreach ($geohash_group as $val) {
 			$store_id = RC_DB::table('store_franchisee')->where('geohash', 'like', $val.'%')->lists('store_id');
-			if (!empty($seller_id)) {
-				$group_store_id = array_merge($group_store_id, $seller_id);
+			if (!empty($store_id)) {
+				$group_store_id = array_merge($group_store_id, $store_id);
 			}
 		}
-	
 		return $group_store_id;
-		
+
 // 		/* 获取当前经纬度的geohash值*/
 // 		$geohash_code = $geohash->encode($filter['location']['latitude'] , $filter['location']['longitude']);
 // 		$geohash_code = substr($geohash_code, 0, 5);
