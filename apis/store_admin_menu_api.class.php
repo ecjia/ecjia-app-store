f090585c5d6860da655e69fdc8441be1717b6780
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-18 19:40:09 +0800

    菜单修改

--- apis/store_admin_menu_api.class.php
+++ apis/store_admin_menu_api.class.php
@@ -8,13 +8,12 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class store_admin_menu_api extends Component_Event_Api {
 
     public function call(&$options) {
-        $menus = ecjia_admin::make_admin_menu('store', RC_Lang::get('store::store.store_manage'), '', 3);
+        $menus = ecjia_admin::make_admin_menu('store', RC_Lang::get('store::store.store_manage'), '', 2);
         
         $submenus = array(
             ecjia_admin::make_admin_menu('01', RC_Lang::get('store::store.store_affiliate'), RC_Uri::url('store/admin/init'), 1)->add_purview('store_affiliate_manage'),
         	ecjia_admin::make_admin_menu('02', RC_Lang::get('store::store.preaudit'), RC_Uri::url('store/admin_preaudit/init'), 2)->add_purview('store_preaudit_manage'),
         	ecjia_admin::make_admin_menu('03', RC_Lang::get('store::store.category'), RC_Uri::url('store/admin_store_category/init'), 3)->add_purview(''),
-        	ecjia_admin::make_admin_menu('04', RC_Lang::get('store::store.commission'), RC_Uri::url('store/admin_commission/init'), 4)->add_purview(''),
         	ecjia_admin::make_admin_menu('05', RC_Lang::get('store::store.percent'), RC_Uri::url('store/admin_percent/init'), 5)->add_purview(''),
         	ecjia_admin::make_admin_menu('divider', '', '', 6)->add_purview(''),
         	ecjia_admin::make_admin_menu('07', RC_Lang::get('store::store.config'), RC_Uri::url('store/admin_config/init'), 7)->add_purview(''),
