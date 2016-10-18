e4125f66c9bcfbab24088527270f1ca50e5dd48d
许文彪 <huangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-18 16:32:51 +0800

    删除待审核页面筛选样式

--- templates/admin/store_preaudit_list.dwt.php
+++ templates/admin/store_preaudit_list.dwt.php
@@ -12,46 +12,14 @@
 	</h3>
 </div>
 
-<!-- <div class="row-fluid" > -->
-	<ul class="nav nav-pills">
-		<li class="{if !$smarty.get.type}active{/if}">
-			<a class="data-pjax" href="{RC_Uri::url('goods/admin/init')}{if $filter.cat_id}&cat_id={$filter.cat_id}{/if}{if $filter.brand_id}&brand_id={$filter.brand_id}{/if}{if $filter.intro_type}&intro_type={$filter.intro_type}{/if}{if $filter.merchant_keywords}&merchant_keywords={$filter.merchant_keywords}{/if}{if $filter.keywords}&keywords={$filter.keywords}{/if}">{lang key='store::store.preaudit_list'} 
-				<span class="badge badge-info">{$goods_list.filter.count_goods_num}</span>
-			</a>
-		</li>
-		
-		<li class="{if $smarty.get.type eq 1}active{/if}">
-			<a class="data-pjax" href='{RC_Uri::url("goods/admin/init", "type=1{if $filter.cat_id}&cat_id={$filter.cat_id}{/if}{if $filter.brand_id}&brand_id={$filter.brand_id}{/if}{if $filter.intro_type}&intro_type={$filter.intro_type}{/if}{if $filter.merchant_keywords}&merchant_keywords={$filter.merchant_keywords}{/if}{if $filter.keywords}&keywords={$filter.keywords}{/if}")}'>{lang key='store::store.check'}
-				<span class="badge badge-info use-plugins-num">{$goods_list.filter.count_on_sale}</span>
-			</a>
-		</li>
-		
-		<li class="{if $smarty.get.type eq 2}active{/if}">	
-			<a class="data-pjax" href='{RC_Uri::url("goods/admin/init", "type=2{if $filter.cat_id}&cat_id={$filter.cat_id}{/if}{if $filter.brand_id}&brand_id={$filter.brand_id}{/if}{if $filter.intro_type}&intro_type={$filter.intro_type}{/if}{if $filter.merchant_keywords}&merchant_keywords={$filter.merchant_keywords}{/if}{if $filter.keywords}&keywords={$filter.keywords}{/if}")}'>{lang key='store::store.not_check'}
-				<span class="badge badge-info unuse-plugins-num">{$goods_list.filter.count_not_sale}</span>
-			</a>
-		</li>
-  	</ul>
-		
+<div class="row-fluid" >
 	<div class="row-fluid batch">
 		<form class="f_r form-inline" action="{$search_action}" name="searchForm" method="post">
 			<input type="text" name="keywords" placeholder="{lang key='store::store.pls_name'}" value="{$store_list.filter.keywords}"/>
 			<input class="btn search_store" type="submit" value="{lang key='store::store.serach'}"/>
 	  	</form>
-		<div class="btn-group f_l m_r5">
-			<form class="f_r form-inline" action="{RC_Uri::url('goods/admin/init')}{if $smarty.get.type}&type={$smarty.get.type}{/if}" method="post" name="filterForm">
-				<div class="screen f_r">
-					<!-- 审核 -->
-					<select class="w100" name="cat_id">
-						<option value="0">{lang key='store::store.preaudit_list'}</option>
-						<!-- {$cat_list} -->
-					</select>
-					<button class="btn screen-btn" type="button">{lang key='store::store.filter'}</button>
-				</div>
-			</form> 
-	 	</div>
   	</div>
-<!-- </div> -->
+</div>
   	
 <div class="row-fluid">
 	<div class="span12">
