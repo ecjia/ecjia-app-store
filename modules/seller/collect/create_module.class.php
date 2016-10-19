4048d06e9107157dcdd9c67c3ed8ae175f7338c0
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-19 15:18:15 +0800

    修复收藏店铺

--- modules/seller/collect/create_module.class.php
+++ modules/seller/collect/create_module.class.php
@@ -6,29 +6,28 @@ defined('IN_ECJIA') or exit('No permission resources.');
  *
  */
 class create_module extends api_front implements api_interface {
-    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
-    	
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+
     	$this->authSession();
 		$user_id = EM_Api::$session['uid'];
 		$seller_id = $this->requestData('seller_id');
-		
+
 		if (empty($seller_id)) {
 			return new ecjia_error( 'invalid_parameter', RC_Lang::get ('system::system.invalid_parameter' ));
 		}
-		$cs_db = RC_Model::model('seller/collect_store_model');
-		$row = $cs_db->find(array('user_id' => $user_id, 'seller_id' => $seller_id));
+		$cs_db = RC_Model::model('store/collect_store_model');
+		$row = $cs_db->find(array('user_id' => $user_id, 'store_id' => $seller_id));
 		if (!empty($row)) {
 			$result = new ecjia_error('is_collected', __('该店铺已收藏！'));
 			return $result;
 		}
-		
-		$ssi_db = RC_Model::model('seller/seller_shopinfo_model');
+
+		// $ssi_db = RC_Model::model('store/seller_shopinfo_model');
 // 		$msi_dbview = RC_Loader::load_app_model('merchants_shop_information_viewmodel', 'seller');
 		$where = array();
 		$where['status'] = 1;
-// 		$where['msi.merchants_audit'] = 1;
 		$where['id'] = $seller_id;
-		$count = $ssi_db->where($where)->count();
+		$count = RC_DB::table('store_franchisee')->where('status', '1')->where('store_id', $seller_id)->count();
 		if ($count == 0 ) {
 			$result = new ecjia_error('shop_error', __('店铺不存在！'));
 			return $result;
@@ -36,14 +35,13 @@ class create_module extends api_front implements api_interface {
 		$data = array(
 				'user_id'	=> $user_id,
 				'seller_id' => $seller_id,
-// 				'ru_id'		=> $seller_id,
 				'add_time'	=> RC_Time::gmtime(),
 				'is_attention' => 1,
 		);
 		$cs_db->insert($data);
-		
+
 		return array();
-	}	
+	}
 }
 
 
