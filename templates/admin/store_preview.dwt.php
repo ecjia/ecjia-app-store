ea9dd8fc0ad19ca66a69cd1c3c1cc3803dcd5d87
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-19 18:44:16 +0800

    添加详情页个人企业判断，修复营业注册好传值错误。

--- templates/admin/store_preview.dwt.php
+++ templates/admin/store_preview.dwt.php
@@ -25,6 +25,84 @@
 						<strong>商家信息</strong>
 					</a>
 				</div>
+
+				{if $store.identity_type eq 1}
+				<div class="accordion-body in collapse" id="goods_info_area_submit">
+					<table class="table table-oddtd m_b0">
+						<tbody class="first-td-no-leftbd">
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.store_title_lable'}</strong></div></td>
+							<td>{$store.merchants_name}</td>
+							<td><div align="right"><strong>{lang key='store::store.store_keywords_lable'}</strong></div></td>
+							<td>{$store.shop_keyword}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.store_cat_lable'}</strong></div></td>
+							<td>{$store.cat_name}</td>
+							<td><div align="right"><strong>{lang key='store::store.apply_time_lable'}</strong></div></td>
+							<td>{$store.apply_time}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.identity_type_lable'}</strong></div></td>
+							<td>{if $store.identity_type eq 1}个人{else}企业{/if}</td>
+							<td><div align="right"><strong>{lang key='store::store.identity_number_lable'}</strong></div></td>
+							<td>{$store.identity_number}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{t}个人名称：{/t}</strong></div></td>
+							<td colspan="3">{$store.responsible_person}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.bank_name_lable'}</strong></div></td>
+							<td>{$store.bank_name}</td>
+							<td><div align="right"><strong>{lang key='store::store.bank_branch_name_lable'}</strong></div></td>
+							<td>{$store.bank_branch_name}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.bank_account_number_lable'}</strong></div></td>
+							<td>{$store.bank_account_number}</td>
+							<td><div align="right"><strong>{lang key='store::store.bank_account_name_label'}</strong></div></td>
+							<td>{$store.bank_account_name}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.email_lable'}</strong></div></td>
+							<td>{$store.email}</td>
+							<td><div align="right"><strong>{lang key='store::store.contact_lable'}</strong></div></td>
+							<td>{$store.contact_mobile}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.label_province'}</strong></div></td>
+							<td>{$store.province}</td>
+							<td><div align="right"><strong>{lang key='store::store.label_city'}</strong></div></td>
+							<td>{$store.city}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.longitude_lable'}</strong></div></td>
+							<td>{$store.longitude}</td>
+							<td><div align="right"><strong>{lang key='store::store.latitude_lable'}</strong></div></td>
+							<td>{$store.latitude}</td>
+						</tr>
+
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.bank_address_lable'}</strong></div></td>
+							<td colspan="3">{$store.bank_address}</td>
+						</tr>
+						<tr>
+							<td><div align="right"><strong>{lang key='store::store.address_lable'}</strong></div></td>
+							<td colspan="3">{$store.address}</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+				{elseif $store.identity_type eq 2}
 				<div class="accordion-body in collapse" id="goods_info_area_submit">
 					<table class="table table-oddtd m_b0">
 						<tbody class="first-td-no-leftbd">
@@ -107,11 +185,15 @@
 						</tbody>
 					</table>
 				</div>
+				{/if}
 			</div>
 		</div>
 
 		<form class="form-horizontal" id="form-privilege" name="theForm" action="{$form_action}" method="post" enctype="multipart/form-data" >
 			<fieldset>
+				{if $store.identity_type eq 1}
+				<input type="hidden"  name="identity_type" value="{$store.identity_type}" />
+				{elseif $store.identity_type eq 2}
 				<div class="control-group formSep">
 					<label class="control-label">{lang key='store::store.business_licence_pic_lable'}</label>
 					<div class="controls">
@@ -127,6 +209,7 @@
 						</div>
 					</div>
 				</div>
+				{/if}
 
 				<div class="control-group formSep">
 					<label class="control-label">{lang key='store::store.identity_pic_front_lable'}</label>
