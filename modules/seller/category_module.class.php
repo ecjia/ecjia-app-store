de0c9e4e2612c26b3408198c76d2043866bd6997
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-19 11:35:57 +0800

    修改商家分类接口

--- modules/seller/category_module.class.php
+++ modules/seller/category_module.class.php
@@ -2,33 +2,37 @@
 defined('IN_ECJIA') or exit('No permission resources.');
 /**
  * 店铺街分类
- * @author 
+ * @author
  *
  */
 class category_module extends api_front implements api_interface {
-    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
-    	
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+
     	$this->authSession();
 		$location = $this->requestData('location');
-		RC_Loader::load_app_class('store_category', 'seller');
+		RC_Loader::load_app_class('store_category', 'store');
 		//$db_shop = RC_Loader::load_app_model('seller_shopinfo_model', 'seller');
-		$scs_view = RC_Model::model('seller/seller_category_shopinfo_viewmodel');
+		$scs_view = RC_Model::model('store/store_franchisee_viewmodel');
 		/* 根据经纬度查询附近店铺*/
 // 		if (is_array($location) && isset($location['latitude']) && isset($location['longitude'])) {
 // 			$request = array('location' => $location);
 // 			$geohash = RC_Loader::load_app_class('geohash', 'shipping');
 // 			$where_geohash = $geohash->encode($location['latitude'] , $location['longitude']);
 // 			$where_geohash = substr($where_geohash, 0, 5);
-					
+
 // 			$where['geohash'] = array('like' => "%$where_geohash%");
 // 		}
 
-		$shop_cat = $scs_view->where(array('s.cat_id' => array('gt' => 0), 'is_show' => 1))->group('s.cat_id')->get_field('s.cat_id', true);
-		$where['c.cat_id'] = $shop_cat;
+        // $shop_cat = $scs_view->where(array('s.cat_id' => array('gt' => 0), 'is_show' => 1))->
+        // group('s.cat_id')->get_field('s.cat_id', true);
+        // $where['c.cat_id'] = $shop_cat;
 
+		$shop_cat = $scs_view->join('store_category')->field('ssi.cat_id')->where(array('ssi.cat_id' => array('gt' => 0), 'status' => 1))->group('ssi.cat_id')->select();
+        $shop_cat = array_column($shop_cat, 'cat_id');
+        $where['sc.cat_id'] = $shop_cat;
 		$cat_all = store_category::get_categories_tree($where);
 		$cat_all = array_merge($cat_all);
-		
+
 		if (!empty($cat_all)) {
 			foreach($cat_all as $key => $val) {
 				$categoryStore[$key]['id'] = $val['id'];
@@ -39,7 +43,7 @@ class category_module extends api_front implements api_interface {
 								'id'     => $v['id'],
 								'name'   => $v['name'],
 						);
-							
+
 						if( !empty($v['cat_id']) ) {
 							foreach($v['cat_id'] as $k1 => $v1) {
 								$categoryStore[$key]['children'][$k1]['children'][] = array(
@@ -50,7 +54,7 @@ class category_module extends api_front implements api_interface {
 						} else {
 							$categoryStore[$key]['children'][$k]['children'] = array();
 						}
-							
+
 						$categoryStore[$key]['children'] = array_merge($categoryStore[$key]['children']);
 					}
 				} else {
@@ -59,8 +63,8 @@ class category_module extends api_front implements api_interface {
 			}
 		}
 		return $categoryStore;
-	}	
-		
+	}
+
 }
 
 
