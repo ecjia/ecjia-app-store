0eb124ca46a7bcaea80d0cc8499a4230a345395c
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 21:31:36 +0800

    admin/merchant/info 修复 需完善

--- modules/admin/merchant/info_module.class.php
+++ modules/admin/merchant/info_module.class.php
@@ -5,30 +5,27 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author luchongchong
  *
  */
-class info_module implements ecjia_interface
-{
-    public function run(ecjia_api & $api)
-    { 
-		$ecjia = RC_Loader::load_app_class('api_admin', 'api');
-		$ecjia->authadminSession();
-//     	if($_SESSION['ru_id'] > 0) {
-    	if($_SESSION['seller_id'] > 0) {
-			$msi_dbview = RC_Loader::load_app_model('merchants_shop_information_viewmodel', 'seller');
-			$msi_dbview->view = array(
-					'seller_shopinfo' => array(
-						'type'  => Component_Model_View::TYPE_LEFT_JOIN,
-						'alias' => 'ssi',
-						'on'    => 'msi.shop_id = ssi.shop_id ',
-					),
-					'seller_category' => array(
-							'type'  => Component_Model_View::TYPE_LEFT_JOIN,
-							'alias' => 'sc',
-							'on'    => 'ssi.cat_id = sc.cat_id',
-					),
-			);
-			$region = RC_Loader::load_app_model('region_model', 'shipping');
+class info_module extends api_admin implements api_interface {
+    
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+		$this->authadminSession();
+    	if($_SESSION['store_id'] > 0) {
+// 			$msi_dbview = RC_Model::model('store/store_franchisee_model');
+// 			$msi_dbview->view = array(
+// 					'seller_shopinfo' => array(
+// 						'type'  => Component_Model_View::TYPE_LEFT_JOIN,
+// 						'alias' => 'ssi',
+// 						'on'    => 'msi.shop_id = ssi.shop_id ',
+// 					),
+// 					'seller_category' => array(
+// 							'type'  => Component_Model_View::TYPE_LEFT_JOIN,
+// 							'alias' => 'sc',
+// 							'on'    => 'ssi.cat_id = sc.cat_id',
+// 					),
+// 			);
+			$region = RC_Model::model('shipping/region_model');
 			$where = array();
-			$where['ssi.id'] = $_SESSION['seller_id'];
+			/* $where['ssi.id'] = $_SESSION['store_id'];
 			$field ='ssi.*, msi.*, ssi.id as seller_id, ssi.shop_name as seller_name, sc.cat_name, ssi.shop_logo';
 			$info = $msi_dbview->join(array('seller_category', 'seller_shopinfo'))
 								 ->field($field)
@@ -36,22 +33,25 @@ class info_module implements ecjia_interface
 								 ->find();
 			if(substr($info['shop_logo'], 0, 1) == '.') {
 				$info['shop_logo'] = str_replace('../', '/', $info['shop_logo']);
-			}
+			} */
 // 			$is_validated = (!empty($info['create_time']) && !empty($info['confirm_time'])) ? 1 : 0;
-			 
+			
+			$info = RC_DB::table('store_franchisee')->where('store_id', $_SESSION['store_id'])->first();
+			
+			
 			$seller_info = array(
-	    	  		'id'					=> $info['seller_id'],
-	    	  		'seller_name'			=> $info['seller_name'],
-	    	  		'seller_logo'			=> RC_Upload::upload_url().'/'.$info['shop_logo'],
-	    	  		'seller_category'		=> $info['cat_name'],
-	    	  		'seller_telephone'		=> $info['kf_tel'],
+	    	  		'id'					=> $info['store_id'],
+	    	  		'seller_name'			=> $info['merchants_name'],
+	    	  		'seller_logo'			=> /* RC_Upload::upload_url().'/'.$info['shop_logo'] */'',
+	    	  		'seller_category'		=> /* $info['cat_name'] */'',
+	    	  		'seller_telephone'		=> $info['contact_mobile'],//$info['kf_tel'],
 	    	  		'seller_province'		=> $region->where(array('region_id'=>$info['province']))->get_field('region_name'),
 	    	  		'seller_city'			=> $region->where(array('region_id'=>$info['city']))->get_field('region_name'),
-	    	  		'seller_address'		=> $info['shop_address'],
-	    	  		'seller_description'	=> $info['notice'],
-					'validated_status'		=> $info['merchant_status'],
+	    	  		'seller_address'		=> $info['address'],
+	    	  		'seller_description'	=> '',//$info['notice'],
+					'validated_status'		=> $info['status'],
 			);
-			$result = $ecjia->admin_priv('merchant_setinfo');
+// 			$result = $this->admin_priv('merchant_setinfo');
 			if (is_ecjia_error($result)) {
 				$privilege = 1;
 // 				Read & Write   3
@@ -74,15 +74,15 @@ class info_module implements ecjia_interface
 	    	  		'seller_address'		=> ecjia::config('shop_address'),
 	    	  		'seller_description'	=> strip_tags(ecjia::config('shop_notice'))
 			);
-			$result = $ecjia->admin_priv('shop_config');
+			$result = $this->admin_priv('shop_config');
 			if (is_ecjia_error($result)) {
 				$privilege = 1;
 			} else {
 				$privilege = 3;
 			}
 		}
-		EM_Api::outPut($seller_info, null, $privilege);
-// 		return $seller_info;
-    }	
+// 		EM_Api::outPut($seller_info, null, $privilege);
+		return $seller_info;
+    }
     
 }
