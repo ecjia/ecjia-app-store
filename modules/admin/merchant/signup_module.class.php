a76015068f9bb7f56abaa191c66c2e5b1b0455ab
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 11:16:27 +0800

    admin/merchant/signup申请商家入驻接口修改

--- modules/admin/merchant/signup_module.class.php
+++ modules/admin/merchant/signup_module.class.php
@@ -5,37 +5,39 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author will.chen
  *
  */
-class signup_module implements ecjia_interface
-{
-    public function run(ecjia_api & $api)
-    {
-		$responsible_person	= isset($_POST['responsible_person']) ? trim($_POST['responsible_person']) : '';
-		$email				= isset($_POST['email']) ? trim($_POST['email']) : '';
-		$company_name		= isset($_POST['company_name']) ? trim($_POST['company_name']) : '';
-		$mobile				= isset($_POST['mobile']) ? trim($_POST['mobile']) : '';
-		$seller_name		= isset($_POST['seller_name']) ? trim($_POST['seller_name']) : '';
+class signup_module extends api_admin implements api_interface {
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+		$responsible_person = $this->requestData('responsible_person', '');
+		$email 				= $this->requestData('email', '');
+		$company_name		= $this->requestData('company_name', '');
+		$mobile				= $this->requestData('mobile', '');
+		$seller_name		= $this->requestData('seller_name', '');
 		
 		if (empty($responsible_person) || empty($email) || empty($mobile) || empty($seller_name)) {
 			return new ecjia_error(101, '错误的参数提交');
 		}
 		
-		$info = RC_Model::model('merchant/merchants_shop_information_model')->find(array('contact_mobile' => $mobile));
-		if (!empty($info)) {
+		//$info = RC_Model::model('merchant/merchants_shop_information_model')->find(array('contact_mobile' => $mobile));
+		$info_store_preaudit = RC_DB::table('store_preaudit')->where(RC_DB::raw('contact_mobile'), $mobile)->first();
+		$info_store_franchisee = RC_DB::table('store_franchisee')->where(RC_DB::raw('contact_mobile'), $mobile)->first();
+		
+		if (!empty($info_store_preaudit) || !empty($info_store_franchisee)) {
 			return new ecjia_error('already_signup', '您已申请请勿重复申请！');
 		}
 		
 		$merchant_shop_data = array(
 				'responsible_person'	=> $responsible_person,
 				'company_name'			=> $company_name,
+				'merchants_name'		=> $seller_name,
 				'contact_mobile'		=> $mobile,
-				'steps_audit'	=> 1,
-				'create_time'	=> RC_Time::gmtime(),
+				'check_status'			=> 1,
+				'apply_time'			=> RC_Time::gmtime(),
+				'store_id'				=> 0,
 		);
 		
-		$insert_id = RC_Model::model('merchant/merchants_shop_information_model')->insert($merchant_shop_data);
-		
-		RC_Model::model('seller/seller_shopinfo_model')->insert(array('shop_id' => $insert_id));
-		
+		//$insert_id = RC_Model::model('merchant/merchants_shop_information_model')->insert($merchant_shop_data);
+		//RC_Model::model('seller/seller_shopinfo_model')->insert(array('shop_id' => $insert_id));
+		$insert_id = RC_DB::table('store_preaudit')->insertGetId($merchant_shop_data);
 		return array();
     }	
     
