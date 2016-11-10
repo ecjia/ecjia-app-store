989f90e01ac4087af5f20e433aab61736989b052
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-10 18:33:44 +0800

    优化入驻信息获取验证码

--- modules/admin/merchant/validate_module.class.php
+++ modules/admin/merchant/validate_module.class.php
@@ -29,7 +29,17 @@ class validate_module extends api_admin implements api_interface {
 			}
 		}
 
-		if (!empty($validate_code)) {
+		if ($type == 'mobile' && $validate_type == 'signup') {
+            $info_store_preaudit	= RC_DB::table('store_preaudit')->where('contact_mobile', $value)->count();
+			$info_store_franchisee	= RC_DB::table('store_franchisee')->where('contact_mobile', $value)->first();
+			if (!empty($info_store_preaudit)){
+                return new ecjia_error('merchant_checking', '手机号'.$value.'已被申请请确认该账号是否为本人所有');
+            }elseif(!empty($info_store_franchisee)){
+                return new ecjia_error('merchant_exist', '手机号'.$value.'已被申请请确认该账号是否为本人所有');
+            }
+		}
+
+        if (!empty($validate_code)) {
 			/* 判断校验码*/
 			if ($_SESSION['merchant_validate_code'] != $validate_code) {
 				return new ecjia_error('validate_code_error', '校验码错误！');
@@ -39,58 +49,46 @@ class validate_module extends api_admin implements api_interface {
 			return array('message' => '校验成功！');
 		}
 
+        // 发送验证码
+        $result = ecjia_app::validate_application('sms');
+        /* 判断是否有短信app*/
+        if (!is_ecjia_error($result)) {
+            //发送短信
+            $code = rand(100000, 999999);
+            $tpl_name = 'sms_get_validate';
+            $tpl   = RC_Api::api('sms', 'sms_template', $tpl_name);
+            /* 判断短信模板是否存在*/
+            if (!empty($tpl)) {
+// 					if ($validate_type == 'signup') {
+// 						ecjia_api::$controller->assign('action', '申请入驻认证');
+// 					} else {
+// 						ecjia_api::$controller->assign('action', '查询入驻审核进度');
+// 					}
 
-		if ($type == 'mobile') {
+                ecjia_api::$controller->assign('code', $code);
+                ecjia_api::$controller->assign('service_phone', ecjia::config('service_phone'));
 
-            $info_store_preaudit	= RC_DB::table('store_preaudit')->where('contact_mobile', $value)->count();
-			$info_store_franchisee	= RC_DB::table('store_franchisee')->where('contact_mobile', $value)->first();
-			if (!empty($info_store_preaudit)){
-                return new ecjia_error('merchant_checking', '手机号'.$value.'已被申请请确认该账号是否为本人所有');
-            }elseif(!empty($info_store_franchisee)){
-                return new ecjia_error('merchant_exist', '手机号'.$value.'已被申请请确认该账号是否为本人所有');
-            }else{
-                // 发送验证码
-                $result = ecjia_app::validate_application('sms');
-    			/* 判断是否有短信app*/
-    			if (!is_ecjia_error($result)) {
-    				//发送短信
-    				$code = rand(100000, 999999);
-    				$tpl_name = 'sms_get_validate';
-    				$tpl   = RC_Api::api('sms', 'sms_template', $tpl_name);
-    				/* 判断短信模板是否存在*/
-    				if (!empty($tpl)) {
-    // 					if ($validate_type == 'signup') {
-    // 						ecjia_api::$controller->assign('action', '申请入驻认证');
-    // 					} else {
-    // 						ecjia_api::$controller->assign('action', '查询入驻审核进度');
-    // 					}
-
-    					ecjia_api::$controller->assign('code', $code);
-    					ecjia_api::$controller->assign('service_phone', ecjia::config('service_phone'));
-
-    					$content = ecjia_api::$controller->fetch_string($tpl['template_content']);
-    					$options = array(
-    							'mobile' 		=> $value,
-    							'msg'			=> $content,
-    							'template_id' 	=> $tpl['template_id'],
-    					);
-                        $time = RC_Time::gmtime();
-    					$response = RC_Api::api('sms', 'sms_send', $options);
-    				}
-
-                    /* 判断是否发送成功*/
-            		if (isset($response) && $response === true) {
-            			$time = RC_Time::gmtime();
-                        $_SESSION['merchant_validate_code'] = $code;
-                        $_SESSION['merchant_validate_mobile'] = $value;
-                        $_SESSION['merchant_validate_expiry'] = $time + 1800;//设置有效期30分钟
-            			return array('message' => '验证码发送成功！');
-            		} else {
-            			return new ecjia_error('send_code_error', __('验证码发送失败！'));
-            		}
-    			}
+                $content = ecjia_api::$controller->fetch_string($tpl['template_content']);
+                $options = array(
+                        'mobile' 		=> $value,
+                        'msg'			=> $content,
+                        'template_id' 	=> $tpl['template_id'],
+                );
+                $time = RC_Time::gmtime();
+                $response = RC_Api::api('sms', 'sms_send', $options);
             }
-		}
+        }
+
+        /* 判断是否发送成功*/
+        if (isset($response) && $response === true) {
+            $time = RC_Time::gmtime();
+            $_SESSION['merchant_validate_code'] = $code;
+            $_SESSION['merchant_validate_mobile'] = $value;
+            $_SESSION['merchant_validate_expiry'] = $time + 1800;//设置有效期30分钟
+            return array('message' => '验证码发送成功！');
+        } else {
+            return new ecjia_error('send_code_error', __('验证码发送失败！'));
+        }
     }
 
 }
