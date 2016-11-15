1984aab878a93c94bd62ef0a423c436119c3f782
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-15 19:21:28 +0800

    api修改入驻信息更新审核状态

--- modules/admin/account/validate_module.class.php
+++ modules/admin/account/validate_module.class.php
@@ -120,6 +120,9 @@ class validate_module extends api_admin implements api_interface {
 		}
 
 		$data['identity_status'] = 1;
+		if ($merchant_info['identity_status'] != 2) {
+		    RC_DB::table('store_franchisee')->where('store_id', $_SESSION['store_id'])->update(array('identity_status' => 1));
+		}
 		$data['apply_time'] = RC_Time::gmtime();
 		unset($merchant_info['manage_mode']);unset($merchant_info['status']);unset($merchant_info['shop_close']);unset($merchant_info['apply_time']);
 		unset($merchant_info['confirm_time']);unset($merchant_info['identity_status']);unset($merchant_info['percent_id']);unset($merchant_info['sort_order']);
