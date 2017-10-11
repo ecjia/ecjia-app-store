4ee22e61996f50f4b7ab951a51b82daf01e2c628
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-10-11 16:53:52 +0800

    merchant goods category 更新

--- modules/merchant/goods/category_module.class.php
+++ modules/merchant/goods/category_module.class.php
@@ -55,7 +55,7 @@ class category_module extends api_front implements api_interface {
 
     	$this->authSession();
 		$seller_id = $this->requestData('seller_id');
-		$store_id = empty($store_id) ? $_SESSION['store_id'] : $store_id;
+		$seller_id = empty($seller_id) ? $_SESSION['store_id'] : $seller_id;
 		if (empty($seller_id)) {
 			return new ecjia_error('invalid_parameter', RC_Lang::get ('system::system.invalid_parameter' ));
 		}
