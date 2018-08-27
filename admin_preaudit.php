b1fef8953c21f67920fe9c3258ebc3cba878592f
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-27 17:14:59 +0800

    修复store selectraw

--- admin_preaudit.php
+++ admin_preaudit.php
@@ -671,7 +671,7 @@ class admin_preaudit extends ecjia_admin {
 		$page = new ecjia_page($count, $page_size, 5);
 		$data = $db_store_franchisee
         		->leftJoin('store_category as sc', RC_DB::raw('sp.cat_id'), '=', RC_DB::raw('sc.cat_id'))
-        		->selectRaw('sp.id,sp.merchants_name,sp.merchants_name,sp.responsible_person,sp.apply_time,sp.company_name,sp.contact_mobile,sc.cat_name')
+        		->select(RC_DB::raw('sp.id,sp.merchants_name,sp.merchants_name,sp.responsible_person,sp.apply_time,sp.company_name,sp.contact_mobile,sc.cat_name'))
         		->orderby('id', 'asc')//不可修改，先提交先审核，后提交后审核
         		->take($page->page_size)
         		->skip($page->start_id-1)
