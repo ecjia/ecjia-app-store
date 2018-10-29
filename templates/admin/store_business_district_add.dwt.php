5121b0e4f67c86aa309e98247efbbe034107eff3
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-29 11:12:43 +0800

    优化js、添加经营地区样式

--- templates/admin/store_business_district_add.dwt.php
+++ templates/admin/store_business_district_add.dwt.php
@@ -7,23 +7,21 @@
 	<form class="form-horizontal" method="post" action="{url path='store/admin_store_business_city/insert_district'}" name="Form">
 		<fieldset>
 			<div class="row-fluid priv_list">
-				<div style="max-height:335px;overflow-y: scroll;">
-					<div class="control-group formSep">
-						<label class="control-label control-label-new">所属经营城市名：</label>
-						<div class="controls">
-							<span class="parent_name">{$business_city_info.business_city_name}</span>
-						</div>
+				<div class="control-group formSep">
+					<label class="control-label control-label-new">所属经营城市名：</label>
+					<div class="controls">
+						<span class="parent_name">{$business_city_info.business_city_name}</span>
 					</div>
-					
-					<div class="control-group formSep">
-						<label class="control-label control-label-new">选择经营地区：</label>
-						<div class="controls">
-							<!-- {foreach from=$district_list item=region key=id} -->
-							<div class="choose">
-								<label><input class="checkbox"  name="region_id[]" type="checkbox" value="{$region.region_id}" {if $region.cando eq 1} checked="checked"{/if} autocomplete="off" />{$region.region_name}</label>
-							</div>
-							<!-- {/foreach} -->
+				</div>
+				
+				<div class="control-group formSep">
+					<label class="control-label control-label-new">选择经营地区：</label>
+					<div class="controls" style="max-height:250px;overflow-y: scroll;">
+						<!-- {foreach from=$district_list item=region key=id} -->
+						<div class="choose">
+							<label><input class="checkbox"  name="region_id[]" type="checkbox" value="{$region.region_id}" {if $region.cando eq 1} checked="checked"{/if} autocomplete="off" />{$region.region_name}</label>
 						</div>
+						<!-- {/foreach} -->
 					</div>
 				</div>
 				<div class="control-group m_t10">
