d55f66b48390d227091d718c20dba7f9768b4b1d
李文浩 <huangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-16 13:42:03 +0800

    调整店铺复制中过程数据类代码

--- classes/StoreDuplicate/StoreDuplicateProgressData.php
+++ classes/StoreDuplicate/StoreDuplicateProgressData.php
@@ -11,14 +11,8 @@ namespace Ecjia\App\Store\StoreDuplicate;
 
 use Royalcms\Component\Support\Str;
 
-class StoreDuplicateStatus
+class StoreDuplicateProgressData
 {
-
-    const STATUS_PROCESSING = 'processing';
-
-    const STATUS_FINISHED = 'finished';
-
-    protected $status;
 
     /**
      * 存储已经完成的复制项
@@ -26,24 +20,6 @@ class StoreDuplicateStatus
      * @var array
      */
     protected $duplicate_items = [];
-
-    /**
-     * @return string
-     */
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    /**
-     * @param string $status
-     * @return StoreDuplicateStatus
-     */
-    public function setStatus($status)
-    {
-        $this->status = $status;
-        return $this;
-    }
 
     /**
      * @return array
@@ -73,7 +49,7 @@ class StoreDuplicateStatus
     public function toArray()
     {
         return [
-            'status' => $this->status,
+            //'status' => $this->status,
             'duplicate_items' => $this->duplicate_items,
         ];
 
