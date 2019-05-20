58b2d1c74dea6af08a22216dc8eab904fdebe93f
李文浩 <huangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-20 11:13:56 +0800

    修改店铺复制中的逻辑判断

--- admin.php
+++ admin.php
@@ -1849,6 +1849,10 @@ class admin extends ecjia_admin
             return $this->showmessage(__('操作失败，当前code无效', 'store'), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
         }
 
+        if ($handle->isCheckFinished()){
+            return $this->showmessage(sprintf(__('%s已复制完成，请不要重复复制', 'store'), $handle->getName()), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+        }
+
         $pjaxurl = RC_Uri::url('store/admin/duplicate_processing', ['store_id' => $store_id, 'source_store_id' => $this->store_info['duplicate_source_store_id']]);
 
         $result = $handle->handleDuplicate();
@@ -1903,6 +1907,43 @@ class admin extends ecjia_admin
         }
     }
 
+    public function test(){
+        $array = RC_DB::table('merchants_category')->where('store_id', 62)->get();
+
+        dump($array);
+        $tree = $this->getTree($array);
+
+        $this->html($tree);
+
+    }
+
+    public function getTree($array, $pid =0, $level = 0){
+
+        //声明静态数组,避免递归调用时,多次声明导致数组覆盖
+        static $list = [];
+        foreach ($array as $key => $value){
+            //第一次遍历,找到父节点为根节点的节点 也就是pid=0的节点
+            if ($value['parent_id'] == $pid){
+                //父节点为根节点的节点,级别为0，也就是第一级
+                $value['level'] = $level;
+                //把数组放到list中
+                $list[] = $value;
+                //把这个节点从数组中移除,减少后续递归消耗
+                unset($array[$key]);
+                //开始递归,查找父ID为该节点ID的节点,级别则为原级别+1
+                $this->getTree($array, $value['cat_id'], $level+1);
+
+            }
+        }
+        return $list;
+    }
+//输出效果
+    public function html($tree)
+    {
+        foreach ($tree as $key => $value) {
+            echo str_repeat('|----', $value['level']), $value['cat_name'].'<br />';
+        }
+    }
 
 }
 
